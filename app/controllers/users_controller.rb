539205ea5ca1161e9d86715d4db31e0205142e6e
theodoros-git <kossitheodore@yahoo.com>
2021-11-25 17:04:30 +0100

    correction

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -69,7 +69,7 @@ class UsersController < ApplicationController
     end
 
     def user_params
-      params.require(:user).permit(:email, :password, :password_confirmation, :is_admin)
+      params.require(:user).permit(:name, :email, :password, :password_confirmation, :is_admin)
     end
 
   end
