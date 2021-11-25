c38a1cf7cea9d95dd930ee5d1c607c2343a48aa8
theodoros-git <kossitheodore@yahoo.com>
2021-11-25 10:06:40 +0100

    make it better

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,3 +8,7 @@
 
 user = User.create(name: "test_user", email: "text@example.com", is_admin: true, password: "password")
 tasks = Task.all
+
+tasks.each do |task|
+    task.update(user_id: User.last.id)
+end
